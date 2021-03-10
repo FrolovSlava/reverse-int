6aa22c60ae06838a4548ac54fcdaf6ab628e0eff
FrolovSlava <7566724@gmail.com>
2021-03-10 12:38:32 +0300

    Create tasc Reverse Int
    Create tasc Reverse Int in RS school

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function reverse (n) {
-  
-}
+
+    if (n<0){
+        n *= -1;
+    }
+    let num = 0;
+    while (n != 0){
+        num *= 10;
+        num += n % 10;
+        n = Math.trunc(n/10);
+    }
+    return num;
+
+
+
+
+
+
+
+
+    /*let mas= String(n).split('');
+    
+    
+    let array = [];
+    for( let index = mas.length; index>0; index--){
+        array.push(mas[index])
+    }
+    console.log(array.join());
+    return array.join()*/
+  }
